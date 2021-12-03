9f27e7927775b5c6ee3b92128cc90de289eaddce
Techguy791 <69944483+Techguy791@users.noreply.github.com>
2021-12-03 16:13:03 -0700

    Add files via upload
    v1.1 release

--- API Test.py
+++ API Test.py
@@ -19,31 +19,27 @@ def buttonClick():
         button.config(text = "An error occured!")
     
         
-def main():
-    nameLabel = tk.Label(window, text="Scratch Username:")
-    nameEntry = tk.Entry(window)
+nameLabel = tk.Label(window, text="Scratch Username:")
+nameEntry = tk.Entry(window)
 
-    passLabel = tk.Label(window, text="Scratch Password:")
-    passEntry = tk.Entry(window)
+passLabel = tk.Label(window, text="Scratch Password:")
+passEntry = tk.Entry(window)
 
-    targetLabel = tk.Label(window, text="Send a comment to:")
-    targetEntry = tk.Entry(window)
+targetLabel = tk.Label(window, text="Send a comment to:")
+targetEntry = tk.Entry(window)
 
-    commentLabel = tk.Label(window, text="Comment:")
-    commentEntry = tk.Entry(window)
+commentLabel = tk.Label(window, text="Comment:")
+commentEntry = tk.Entry(window)
 
-    button = tk.Button(window, text = "Send Comment", command=buttonClick)
+button = tk.Button(window, text = "Send Comment", command=buttonClick)
 
-    nameLabel.pack()
-    nameEntry.pack()
-    passLabel.pack()
-    passEntry.pack()
-    targetLabel.pack()
-    targetEntry.pack()
-    commentLabel.pack()
-    commentEntry.pack()
-    button.pack()
-    window.mainloop()
-
-if __name__ == "__main__":
-    main()
+nameLabel.pack()
+nameEntry.pack()
+passLabel.pack()
+passEntry.pack()
+targetLabel.pack()
+targetEntry.pack()
+commentLabel.pack()
+commentEntry.pack()
+button.pack()
+window.mainloop()
